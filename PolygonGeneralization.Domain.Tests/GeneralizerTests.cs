514000acd8ff197ea4284738703c68230259cb4b
ponomNikita <ponom.nikita@gmail.com>
2017-12-10 21:06:35 +0300

    GetConvexHull method with tests

---
+++ PolygonGeneralization.Domain.Tests/GeneralizerTests.cs
@@ -0,0 +1,137 @@
+﻿
+using System.Linq;
+using NUnit.Framework;
+using PolygonGeneralization.Domain.Models;
+
+namespace PolygonGeneralization.Domain.Tests
+{
+    [TestFixture]
+    public class GeneralizerTests
+    {
+        private readonly Generalizer _generalizer = new Generalizer();
+
+        [Test]
+        public void GetConvexHullOfSeparatedPolygonsTests()
+        {
+            var polygonA = new Polygon(new Path(new []
+            {
+                new Point(4, 4),
+                new Point(0, 4),
+                new Point(0, 0),
+                new Point(4, 0),
+            }));
+
+            var polygonB = new Polygon(new Path(new[]
+            {
+                new Point(9, 4),
+                new Point(5, 4),
+                new Point(5, 0),
+                new Point(9, 0),
+            }));
+
+            var expecetdPointArray = new[]
+            {
+                new Point(0, 0),
+                new Point(9, 0),
+                new Point(9, 4),
+                new Point(0, 4),
+            };
+
+            var actual = _generalizer.GetConvexHull(polygonA, polygonB);
+
+            var actualPointsArray = actual.Paths.SelectMany(p => p.Points).ToArray();
+
+            CollectionAssert.AreEqual(expecetdPointArray, actualPointsArray);
+        }
+
+        [Test]
+        public void GetConvexHullOfFourSeparatedPolygonsTests()
+        {
+            var polygonA = new Polygon(new Path(new[]
+            {
+                new Point(4, 3),
+                new Point(0, 3),
+                new Point(0, 0),
+                new Point(4, 0),
+            }));
+
+            var polygonB = new Polygon(new Path(new[]
+            {
+                new Point(9, 4),
+                new Point(5, 4),
+                new Point(5, 1),
+                new Point(9, 1),
+            }));
+
+            var polygonC = new Polygon(new Path(new[]
+            {
+                new Point(7, 7),
+                new Point(2, 7),
+                new Point(2, 4),
+                new Point(7, 4),
+            }));
+
+            var polygonD = new Polygon(new Path(new[]
+            {
+                new Point(11, 8),
+                new Point(8, 8),
+                new Point(8, 5),
+                new Point(11, 5),
+            }));
+
+            var expecetdPointArray = new[]
+            {
+                new Point(0, 0),
+                new Point(4, 0),
+                new Point(9, 1),
+                new Point(11, 5),
+                new Point(11, 8),
+                new Point(8, 8),
+                new Point(2, 7),
+                new Point(0, 3),
+            };
+
+            var actual = _generalizer.GetConvexHull(polygonA, polygonB, polygonC, polygonD);
+
+            var actualPointsArray = actual.Paths.SelectMany(p => p.Points).ToArray();
+
+            CollectionAssert.AreEqual(expecetdPointArray, actualPointsArray);
+        }
+
+        [Test]
+        public void GetConvexHullOfTwoSquares()
+        {
+            var polygonA = new Polygon(new Path(new[]
+            {
+                new Point(4, 4),
+                new Point(0, 4),
+                new Point(0, 0),
+                new Point(4, 0),
+            }));
+
+            var polygonB = new Polygon(new Path(new[]
+            {
+                new Point(8, 8),
+                new Point(2, 8),
+                new Point(2, 2),
+                new Point(8, 2),
+            }));
+
+            var expecetdPointArray = new[]
+             {
+                new Point(0, 0),
+                new Point(4, 0),
+                new Point(8, 2),
+                new Point(8, 8),
+                new Point(2, 8),
+                new Point(0, 4)
+            };
+
+            var actual = _generalizer.GetConvexHull(polygonA, polygonB);
+
+            var actualPointsArray = actual.Paths.SelectMany(p => p.Points).ToArray();
+
+            CollectionAssert.AreEqual(expecetdPointArray, actualPointsArray);
+        }
+    }
+}
--- PolygonGeneralization.Domain/Generalizer.cs
+++ PolygonGeneralization.Domain/Generalizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PolygonGeneralization.Domain.Interfaces;
 using PolygonGeneralization.Domain.Models;
 
@@ -9,6 +11,54 @@ namespace PolygonGeneralization.Domain
         public List<Polygon> Generalize(Polygon[] polygons, double pxForMeter)
         {
             throw new System.NotImplementedException();
+        }
+
+        public Polygon GetConvexHull(params Polygon[] polygons)
+        {
+            var points = polygons.SelectMany(p => p.Paths).SelectMany(p => p.Points).ToList();
+
+            points.Sort(new PointsByXComparator());
+
+            int n = points.Count, k = 0;
+            Point[] convexHull = new Point[2 * n];
+
+            // Build lower hull
+            for (int i = 0; i < n; ++i)
+            {
+                while (k >= 2 && Cross(convexHull[k - 2], convexHull[k - 1], points[i]) <= 0)
+                    k--;
+                convexHull[k++] = points[i];
+            }
+
+            // Build upper hull
+            for (int i = n - 2, t = k + 1; i >= 0; i--)
+            {
+                while (k >= t && Cross(convexHull[k - 2], convexHull[k - 1], points[i]) <= 0)
+                    k--;
+                convexHull[k++] = points[i];
+            }
+
+            if (k > 1)
+                convexHull = convexHull.Take(k - 1).ToArray();
+
+            return new Polygon(new Path(convexHull));
+        }
+
+        private double Cross(Point o, Point a, Point b)
+        {
+            return (a.X - o.X) * (b.Y - o.Y) - (a.Y - o.Y) * (b.X - o.X);
+        }
+
+        private class PointsByXComparator : IComparer<Point>
+        {
+            public int Compare(Point x, Point y)
+            {
+                if (Math.Abs(x.X - y.X) < Double.Epsilon)
+                {
+                    return x.Y - y.Y > 0 ? 1 : -1;
+                }
+                return x.X - y.X > 0 ? 1 : -1;
+            }
         }
     }
 }
