d5bdf6d64c624c5e420a6eb55ecabda463ab0a93
ponomNikita <ponom.nikita@gmail.com>
2018-04-18 23:00:43 +0300

    ClockwiseOrderComparer fixup

---
+++ PolygonGeneralization.Domain.Tests/ClockwiseOrderComparerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PolygonGeneralization.Domain.Models;
+using PolygonGeneralization.Domain.SimpleClipper;
+
+namespace PolygonGeneralization.Domain.Tests
+{
+    public class ClockwiseOrderComparerTests
+    {
+        private static IEnumerable<object[]> ComparerTestCases => new[]
+        {
+            // Case 1
+            new object[]
+            {
+                new Point[] 
+                {
+                    new Point(1, 10), 
+                    new Point(10, 0),
+                    new Point(10, 10),
+                    new Point(10, 5)
+                },
+                new Point(10, 10) // expected
+            },
+            // Case 2
+            new object[]
+            {
+                new Point[] 
+                {
+                    new Point(-1, 20), 
+                    new Point(-5, 20), 
+                    new Point(10, 0),
+                    new Point(10, 10),
+                    new Point(0, 20), 
+                    new Point(10, 5)
+                },
+                new Point(0, 20) // expected
+            },
+            // Case 3
+            new object[]
+            {
+                new Point[] 
+                { 
+                    new Point(0, 20), 
+                    new Point(0, 15), 
+                },
+                new Point(0, 20) // expected
+            },
+            // Case 4
+            new object[]
+            {
+                new Point[] 
+                { 
+                    new Point(-5, 10), 
+                    new Point(0, 20), 
+                },
+                new Point(0, 20) // expected
+            }
+        };
+        
+        [TestCaseSource(nameof(ComparerTestCases))]
+        public void ComparerTests(Point[] points, Point expected)
+        {
+            var sut = new ClockwiseOrderComparer(new Point(0, 0), new Point(0, 10));
+            Array.Sort(points, sut);
+            
+            Assert.AreEqual(expected, points.First());
+        }
+
+        private static IEnumerable<object[]> ComparerTestCases2 => new[]
+        {
+            new object[] { new Point(-5, -5), new Point(-5, 0) },
+            new object[] { new Point(-5, -5), new Point(-4, -4) },
+            new object[] { new Point(7, 7), new Point(6, 6) },
+            new object[] { new Point(-5, 0), new Point(0, -5) },
+            new object[] { new Point(5, 0), new Point(0, -5) },
+            new object[] { new Point(5, 0), new Point(4, 0) },
+            new object[] { new Point(-6, 0), new Point(-5, 0) },
+            new object[] { new Point(-3, 3), new Point(4, -1) },
+        };
+        
+        
+        [TestCaseSource(nameof(ComparerTestCases2))]
+        public void ComparerTests2(Point a, Point b)
+        {
+            var sut = new ClockwiseOrderComparer(new Point(5, 5), new Point(0, 0));
+
+            var actual = sut.Compare(a, b);
+            
+            Assert.True(actual < 0);
+        }
+
+    }
+}
---
+++ PolygonGeneralization.Infrastructure/Commands/BaseCommandWithResult.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using PolygonGeneralization.Domain.Interfaces;
+using PolygonGeneralization.Infrastructure.Logger;
+
+namespace PolygonGeneralization.Infrastructure.Commands
+{
+    public abstract class BaseCommandWithResult<TResult>: ICommand<TResult>
+    {
+        private readonly ILogger _logger = LoggerFactory.Create();
+        private readonly Stopwatch _stopWatch = new Stopwatch();
+        private string _time;
+
+        public TResult Execute()
+        {
+            if (UseLogging)
+                _logger.Log($"{CommandName} started");
+
+            _stopWatch.Reset();
+            _stopWatch.Start();
+
+            var result = HandleImpl();
+
+            _stopWatch.Stop();
+            _time = _stopWatch.Elapsed.ToString("mm\\:ss\\.ff");
+
+            if (UseLogging)
+                _logger.Log($"Done {_time}");
+
+            return result;
+        }
+
+        public abstract string CommandName { get; }
+        public virtual bool UseLogging => true;
+        public virtual string Time => _time;
+        protected abstract TResult HandleImpl();
+    }
+}
